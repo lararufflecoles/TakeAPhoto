10b7ccbe4b72aeac0c3d1c6f2b313ba7451ee1ee
lararufflecoles <lararuffle@lararuffle.co.uk>
2016-02-10 18:18:12 +0000

    Added camera functionality

--- app/src/main/java/es/rufflecol/lara/takeaphoto/MainActivity.java
+++ app/src/main/java/es/rufflecol/lara/takeaphoto/MainActivity.java
@@ -1,11 +1,19 @@
 package es.rufflecol.lara.takeaphoto;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +24,24 @@ public class MainActivity extends AppCompatActivity {
         takePhotoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePhotoIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePhotoIntent, REQUEST_IMAGE_CAPTURE);
+                }
             }
         });
+
+        imageView = (ImageView) findViewById(R.id.image_view);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
     }
 }
